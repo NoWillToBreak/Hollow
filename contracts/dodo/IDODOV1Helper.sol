74c8d21bba00de17cfcfda72420c32704e3d0e32
19mbp-dengjie <dengjie@yinshantech.cn>
2023-10-01 16:18:24 +0800

    add encode v2

--- contracts/dodo/IDODOV1Helper.sol
+++ contracts/dodo/IDODOV1Helper.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.5;
+pragma abicoder v2;
 interface IDODOV1Helper {
     function querySellQuoteToken(
         address dodoV1Pool,
--- contracts/pancake/v3/IV3SwapRouter.sol
+++ contracts/pancake/v3/IV3SwapRouter.sol
@@ -1,3 +1,7 @@
+
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.5;
+pragma abicoder v2;
 interface IV3SwapRouter {
     struct ExactInputSingleParams {
         address tokenIn;
